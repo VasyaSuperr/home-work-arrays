b09b175d1f7e7410e56b42bf47f26617fed39c10
VasyaSuperr <yenakii.vasyl@gmail.com>
2024-05-30 14:27:43 +0300

    Task 0

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,10 @@
-alert("Hello world");
+console.log("Task 0");
+
+const arraySize = 20;
+let randomArray = [];
+
+for (let i = 0; i < arraySize; i++) {
+  randomArray.push(parseInt(Math.random() * 100));
+}
+
+console.log("randomArray", randomArray);
