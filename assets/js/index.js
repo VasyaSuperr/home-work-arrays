dc2ac0e10d15c68128ab734c1cb47d78008b8fff
VasyaSuperr <yenakii.vasyl@gmail.com>
2024-05-30 14:59:08 +0300

    Task 2 and Task 3

--- assets/js/index.js
+++ assets/js/index.js
@@ -22,3 +22,13 @@ console.log("Deleted first element: \n", randomArray);
 
 randomArray.unshift(parseInt(Math.random() * 100));
 console.log("Added element at the beginning: \n", randomArray);
+
+console.log("=========================Task 2=========================");
+console.log("Array lenght = ", randomArray.length);
+
+console.log("=========================Task 3=========================");
+// const randomArrayCopy = Array.from(randomArray);
+const randomArrayCopy = [...randomArray];
+randomArrayCopy[1] = 5;
+console.log("New copied array", randomArrayCopy);
+console.log("Old array", randomArray);
