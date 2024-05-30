f4643379f35417278dd1c0915c9db6a5b049a9a8
VasyaSuperr <yenakii.vasyl@gmail.com>
2024-05-30 14:50:22 +0300

    Task 1

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,4 @@
-console.log("Task 0");
+console.log("=========================Task 0=========================");
 
 const arraySize = 20;
 let randomArray = [];
@@ -8,3 +8,17 @@ for (let i = 0; i < arraySize; i++) {
 }
 
 console.log("randomArray", randomArray);
+
+console.log("=========================Task 1=========================");
+
+randomArray.pop();
+console.log("Deleted last element: \n", randomArray);
+
+randomArray.push(parseInt(Math.random() * 100));
+console.log("Added an element to the end: \n", randomArray);
+
+randomArray.shift();
+console.log("Deleted first element: \n", randomArray);
+
+randomArray.unshift(parseInt(Math.random() * 100));
+console.log("Added element at the beginning: \n", randomArray);
